f2642709b08a62081639b25f478c030fb8657d76
GitGangGuy <git@dothomic.de>
2021-03-11 10:22:48 +0100

    feat: added nodemon, tweaked scripts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { Probot } from 'probot';
 
 export default (app: Probot) => {
-	app.on(['issues.opened', 'issues.reopened'], async (context) => {
-		const issueComment = context.issue({
-			body: 'Thanks for opening this issue!',
-		});
-		await context.octokit.issues.createComment(issueComment);
+	// app.on(['issues.opened', 'issues.reopened'], async (context) => {
+	// 	const issueComment = context.issue({
+	// 		body: 'Thanks for opening this issue!',
+	// 	});
+	// 	await context.octokit.issues.createComment(issueComment);
+	// });
+	app.on('issues.assigned', async (context) => {
+		context.log.info(context);
 	});
-	app.onAny(async (context: any) => {
-		context.log.info({ action: context.payload.action, event: context.name });
+	app.onAny(async (context) => {
+		app.log.info({ action: context.payload.action, event: context.name });
 	});
 };
